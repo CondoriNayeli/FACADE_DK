7b77b547b4a0d599ef29efa3fec05cb173e2a389
FaviolaCondori <cfaviola313@gmail.com>
2024-11-11 21:44:49 -0400

    FACADE

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/DonkeyKong_USFX/EnemigoGargola.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef DONKEYKONG_USFX_EnemigoGargola_generated_h
+#error "EnemigoGargola.generated.h already included, missing '#pragma once' in EnemigoGargola.h"
+#endif
+#define DONKEYKONG_USFX_EnemigoGargola_generated_h
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_SPARSE_DATA
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_RPC_WRAPPERS
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEnemigoGargola(); \
+	friend struct Z_Construct_UClass_AEnemigoGargola_Statics; \
+public: \
+	DECLARE_CLASS(AEnemigoGargola, AEnemigos, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/DonkeyKong_USFX"), NO_API) \
+	DECLARE_SERIALIZER(AEnemigoGargola)
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAEnemigoGargola(); \
+	friend struct Z_Construct_UClass_AEnemigoGargola_Statics; \
+public: \
+	DECLARE_CLASS(AEnemigoGargola, AEnemigos, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/DonkeyKong_USFX"), NO_API) \
+	DECLARE_SERIALIZER(AEnemigoGargola)
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEnemigoGargola(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEnemigoGargola) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemigoGargola); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemigoGargola); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEnemigoGargola(AEnemigoGargola&&); \
+	NO_API AEnemigoGargola(const AEnemigoGargola&); \
+public:
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEnemigoGargola(AEnemigoGargola&&); \
+	NO_API AEnemigoGargola(const AEnemigoGargola&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemigoGargola); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemigoGargola); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AEnemigoGargola)
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_PRIVATE_PROPERTY_OFFSET
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_9_PROLOG
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_PRIVATE_PROPERTY_OFFSET \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_SPARSE_DATA \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_RPC_WRAPPERS \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_INCLASS \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_PRIVATE_PROPERTY_OFFSET \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_SPARSE_DATA \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_INCLASS_NO_PURE_DECLS \
+	FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DONKEYKONG_USFX_API UClass* StaticClass<class AEnemigoGargola>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FACADE_Source_DonkeyKong_USFX_EnemigoGargola_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Intermediate/Build/Win64/UE4Editor/Inc/DonkeyKong_USFX/EnemigosFacade.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef DONKEYKONG_USFX_EnemigosFacade_generated_h
+#error "EnemigosFacade.generated.h already included, missing '#pragma once' in EnemigosFacade.h"
+#endif
+#define DONKEYKONG_USFX_EnemigosFacade_generated_h
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_SPARSE_DATA
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_RPC_WRAPPERS
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_RPC_WRAPPERS_NO_PURE_DECLS
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEnemigosFacade(); \
+	friend struct Z_Construct_UClass_AEnemigosFacade_Statics; \
+public: \
+	DECLARE_CLASS(AEnemigosFacade, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/DonkeyKong_USFX"), NO_API) \
+	DECLARE_SERIALIZER(AEnemigosFacade)
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_INCLASS \
+private: \
+	static void StaticRegisterNativesAEnemigosFacade(); \
+	friend struct Z_Construct_UClass_AEnemigosFacade_Statics; \
+public: \
+	DECLARE_CLASS(AEnemigosFacade, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/DonkeyKong_USFX"), NO_API) \
+	DECLARE_SERIALIZER(AEnemigosFacade)
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEnemigosFacade(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEnemigosFacade) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemigosFacade); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemigosFacade); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEnemigosFacade(AEnemigosFacade&&); \
+	NO_API AEnemigosFacade(const AEnemigosFacade&); \
+public:
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEnemigosFacade(AEnemigosFacade&&); \
+	NO_API AEnemigosFacade(const AEnemigosFacade&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEnemigosFacade); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEnemigosFacade); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AEnemigosFacade)
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_PRIVATE_PROPERTY_OFFSET
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_11_PROLOG
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_PRIVATE_PROPERTY_OFFSET \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_SPARSE_DATA \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_RPC_WRAPPERS \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_INCLASS \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_PRIVATE_PROPERTY_OFFSET \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_SPARSE_DATA \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_RPC_WRAPPERS_NO_PURE_DECLS \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_INCLASS_NO_PURE_DECLS \
+	FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h_14_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> DONKEYKONG_USFX_API UClass* StaticClass<class AEnemigosFacade>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FACADE_Source_DonkeyKong_USFX_EnemigosFacade_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
